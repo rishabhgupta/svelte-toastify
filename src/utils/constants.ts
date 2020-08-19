530a319115327c6bfee6ccbca4202c8480cf1061
rishabhgupta <rishabhgupta@msn.com>
2020-08-20 01:55:01 +0530

    refactor: 💡 moves default configuration to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,12 @@
+import successImage from "../assets/check.svg";
+import errorImage from "../assets/error.svg";
+import infoImage from "../assets/info.svg"
+import warningImage from "../assets/warning.svg";
+
+
+/**
+ * Toast Positions
+ */
 export const POSITION = {
     TOP_LEFT: 'top-left',
     TOP_RIGHT: 'top-right',
@@ -7,6 +16,9 @@ export const POSITION = {
     BOTTOM_CENTER: 'bottom-center'
 } as const;
 
+/**
+ * Toast Types
+ */
 export const TYPE = {
     INFO: 'info',
     SUCCESS: 'success',
@@ -14,3 +26,28 @@ export const TYPE = {
     ERROR: 'error',
     DEFAULT: 'default',
 };
+
+/**
+ * Default Configuration Object
+ * @todo add icon for defaul configuration
+ */
+export const DEFAULT_CONFIG = {
+    position: POSITION.BOTTOM_RIGHT,
+    autoClose: 5000,
+    preventDuplicate: false,
+    closeButton: true,
+    className: null,
+    bodyClassName: null,
+    [TYPE.SUCCESS]: {
+        icon: successImage,
+    },
+    [TYPE.ERROR]: {
+        icon: errorImage,
+    },
+    [TYPE.WARNING]: {
+        icon: warningImage,
+    },
+    [TYPE.INFO]: {
+        icon: infoImage,
+    }
+}
